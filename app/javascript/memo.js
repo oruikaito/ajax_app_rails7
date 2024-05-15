7693a2e9ae8cb7d5d64193d5d9a73e81e28771ab
oruikaito <167738611+oruikaito@users.noreply.github.com>
2024-05-15 16:05:38 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,11 +2,11 @@ function post (){
   const form = document.getElementById("form");
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-      const fromData = new FormData(form);
+      const formData = new FormData(form);
       const XHR = new XMLHttpRequest();
       XHR.open("POST", "/posts", true);
       XHR.responseType = "json";
-      XHR.send(FormData);
+      XHR.send(formData);
     });
  };
  
